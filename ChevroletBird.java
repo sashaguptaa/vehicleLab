10610822c1249e4b15b0ca81cd0b871f49641717
VanshZota <77058912+VanshZota@users.noreply.github.com>
2022-03-15 10:31:33 -0400

    Update ChevroletBird.java

--- ChevroletBird.java
+++ ChevroletBird.java
@@ -1,5 +1,69 @@
 package vehicle;
 
-class ChevroletBird extends ElectricCar{
-    
+public class ChevroletBird extends ElectricCar implements Flying {
+    private boolean wingsExtended;
+
+    /**
+     * Chevrolet Birds have a 250 mile range on a full charge. They start with their
+     * wings retracted.
+     */
+    public ChevroletBird(double startingMileage) {
+        super("Chevrolet", "Bird", startingMileage, 250);
+        wingsExtended = false;
+    }
+
+    /**
+     * Defaults mileage to 0.
+     */
+    public ChevroletBird() {
+        super("Chevrolet", "Bird", 250);
+        wingsExtended = false;
+    }
+
+    /**
+     * Returns whether the wings are currently extended.
+     */
+
+    public boolean checkWingsExtended() {
+        return wingsExtended;
+    }
+
+    //Drives just like all other Electric Cars, except make sure that you retract your wings first
+
+    @Override
+    public void drive(double miles) {
+        wingsExtended = false;
+        super.drive(miles);
+    }
+
+    /**
+     * returns true if wings are retracted and the car has enough charge to go that
+     * amount of miles
+     * //TODO ask if this method is doing what its supposed to and if not update testcase 7 in bird driving
+     */
+
+    @Override
+    public boolean canFly(double miles) {
+
+        if (miles < 0) {
+            throw new IllegalArgumentException("Must be positive.");
+        }
+
+        return wingsExtended; return canDrive(miles);
+    }
+
+    @Override
+    public void fly(double miles) {
+        if (miles < 0) {
+            throw new IllegalArgumentException("Must be positive.");
+        } 
+        
+        else if (canDrive > miles) {
+            throw new IllegalArgumentException(String.format("You can only go %.2f miles on your current charge", getRemainingRange()));
+        }
+
+        decreaseCharge(miles); 
+        wingsExtended = true;
+       
+    }
 }
