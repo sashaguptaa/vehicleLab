6c67958ef394976da5e61933cc81344ef5310dc5
Safa-Karagoz <78101103+Safa-Karagoz@users.noreply.github.com>
2022-03-15 10:41:23 -0400

    Add files via upload
    Ford class added

--- FordFrivolous.java
+++ FordFrivolous.java
@@ -1,5 +1,45 @@
 package vehicle;
 
 class FordFrivolous extends GasPoweredCar{
-    
+    /** FordFrivolous has a gas tank of 20 gallons and an MPG of 23.6. */
+    private double startingMileage; 
+    private double mpg; 
+    private int gasTank;
+    public FordFrivolous(double startingMileage){
+        super("Ford", "Frivolous", startingMileage, 23.6, 20);  
+        this.startingMileage = startingMileage; 
+    }
+
+    /** Defaults mileage to 0. */
+    public FordFrivolous(){
+        super("Ford", "Frivolous", 0, 23.6, 20);  
+
+        this.startingMileage = 0; 
+    }
+
+    public void driveAutonomously(double miles){
+        if(miles < 0 || miles > getRemainingRange()){
+            throw new IllegalArgumentException();
+        }
+
+        decreaseFuelLevel(miles * 2);
+        super.addMileage(miles); 
+    }
+
+    public void fly(double miles){
+        if(miles < 0 || miles > getRemainingRange()){
+            throw new IllegalArgumentException();
+        }
+        decreaseFuelLevel(miles * 3);
+    }
+
+    public boolean canFly(double miles){
+        if (miles < 0){
+            throw new IllegalArgumentException(); 
+        }
+        if (miles < getFuelLevel())
+            return true; 
+        
+        return false; 
+    }
 }
