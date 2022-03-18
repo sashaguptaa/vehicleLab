4f467c6ac96a38486adb79bff5bd7971d6af32e0
VanshZota <77058912+VanshZota@users.noreply.github.com>
2022-03-18 10:19:54 -0400

    added testers

---
+++ bcatest/Group3_4Executable.java
@@ -0,0 +1,9 @@
+package bcatest;
+
+/** Source JUnit Executable implementation
+ * Simplified for our introductory testing purposes. */
+
+@FunctionalInterface
+public interface Group3_4Executable {
+    void execute() throws Throwable;
+}
