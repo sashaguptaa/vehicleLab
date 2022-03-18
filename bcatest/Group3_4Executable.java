0500735578a2cceec1bb41f1452a1e8efebd56b6
VanshZota <77058912+VanshZota@users.noreply.github.com>
2022-03-18 10:29:16 -0400

    changed file directory

--- bcatest/Group3_4Executable.java
+++
@@ -1,9 +0,0 @@
-package bcatest;
-
-/** Source JUnit Executable implementation
- * Simplified for our introductory testing purposes. */
-
-@FunctionalInterface
-public interface Group3_4Executable {
-    void execute() throws Throwable;
-}
