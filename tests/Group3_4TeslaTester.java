9a608e17fc5531e55ed6ef15daccea36105979cc
Sasha Gupta <sashagupta75@gmail.com>
2022-03-22 10:26:32 -0400

    test cases

--- tests/Group3_4TeslaTester.java
+++ tests/Group3_4TeslaTester.java
@@ -37,10 +37,17 @@ public class Group3_4TeslaTester extends BCATestScenario{
     public int runTest() {
         TeslaModelZ c1 = new TeslaModelZ();
 
+        // #13
         assertThrows(IllegalArgumentException.class, () -> {c1.drive(-1);}, "Driving mileage cannot be negative.");
+        // #14
+        assertTrue(c1.canDrive(1), "canDrive should be true for positive values");
+        // #15
+        assertFalse(c1.canDrive(341), "Driving mileage should not be greater than 340 miles.")
+        // #16
+        c1.toString();
+        // #17
+        assertThrows(IllegalArgumentException.class, () -> {c1.driveAutonomously(-1);}
 
-        assertTrue(c1.canDrive(30), "canDrive should be true");
-        c1.drive(30);
         assertEquals(c1.getMileage(), 30, .1, "Mileage should be 30 after first drive.");
 
         c1.drive(200);
