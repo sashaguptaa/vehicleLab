94540a209a7649671f05d4e7cd8234f18c0775da
VanshZota <77058912+VanshZota@users.noreply.github.com>
2022-03-23 20:32:38 -0400

    Update Group3_4TeslaTester.java

--- tests/Group3_4TeslaTester.java
+++ tests/Group3_4TeslaTester.java
@@ -5,67 +5,71 @@ import bcatest.BCATestScenario;
 
 public class Group3_4TeslaTester extends BCATestScenario{
      public int runTest() {
-         TeslaModelZ c1 = new TeslaModelZ();
 
+        //new car with no mileage
+         TeslaModelZ c1 = new TeslaModelZ(70);
+         assertEquals(c1.getMileage(), 0, .1, ("Mileage should be 0"));
+
+         //new car with mileage of 1
+        TeslaModelZ c2 = new TeslaModelZ(1);
+        assertEquals(c2.getMileage(), 1, .1, ("Mileage should be 1"));
+
+        //mileage cannot be -1
          assertThrows(IllegalArgumentException.class, () -> {c1.drive(-1);}, "Driving mileage cannot be negative.");
 
-         assertTrue(c1.canDrive(30), "canDrive should be true");
-         c1.drive(30);
-         assertEquals(c1.getMileage(), 30, .1, "Mileage should be 30 after first drive.");
+         //max range is 340; can't drive more miles than that
+         assertThrows(IllegalArgumentException.class, () -> {c1.drive(341);}, "Not enough range.");
+
+         //car should be able to drive 10 miles
+         assertTrue(c1.canDrive(10), "canDrive should be true");
+
+         c1.drive(10);
+         //mileage should now be 10 after driving ten miles
+         assertEquals(c1.getMileage(), 10, .1, "Mileage should be 10 after first drive.");
+
+         //remaining range should go down by 10 (330)
+         assertEquals(c1.getRemainingRange(), 330, .1, ("Remaining range should go down by 10"));
 
          c1.drive(200);
-         assertEquals(c1.getMileage(), 230, .1, "Mileage should be 230 after second drive.");
+         //car mileage should go down by 200
+         assertEquals(c1.getMileage(), 130, .1, "Mileage should be 130 after second drive.");
 
-         assertEquals(c1.getRemainingRange(), c1.getFuelCapacity() * c1.getMPG() - 230, .1, "Remaining range of car not correct after driving twice.");
+         c1.recharge();
+         //remaining range should become
+         assertEquals(c1.getRemainingRange(), 340, .1, ("Remaining range should be 340"));
 
-         assertFalse(c1.canDrive(252), "Driving 252 should fail.");
-         assertTrue(c1.canDrive(251), "Driving 251 should succeed.");
+         c1.getMaxRange();
+         //max range should be 340
+         assertEquals(c1.getMaxRange(), 340, .1, ("Max range is 340"));
 
-         c1.drive(251);
-         assertEquals(c1.getMileage(), 481, .1, "Mileage should be 481 after third drive.");
+         //return model and modelNum correctly
+         assertEquals(c1.getMake(), "Tesla", "the make is 'Tesla', not " + c1.getMake());
+         assertEquals(c1.getModelNum(), 70, "the model number is 70, not " + c1.getModelNum());
+         assertEquals(c1.getModel(), "Z70", "the model should be Z70, not " + c1.getModel());
 
-         assertThrows(IllegalArgumentException.class, () -> {c1.drive(5);}, "Driving beyond empty should fail.");
+         //break
+
+        TeslaModelZ c3 = new TeslaModelZ(70);
+
+        assertTrue(c3.canDrive(1), "canDrive should be true for positive values");
+
+        assertFalse(c3.canDrive(341), "Driving mileage should not be greater than 340 miles.");
+        c3.toString();
+
+        assertThrows(IllegalArgumentException.class, () -> {c1.driveAutonomously(-1);}, "Self Driving miles cannot be negative.");
+        c3.driveAutonomously(350);
+
+        assertEquals(c3.getMileage(), 340, .1, "Mileage should be 340 after first drive because battery will die.");
+
+        int[] L = {-1, 9, 8};
+        assertThrows(IllegalArgumentException.class, () -> {c3.roadTrip(L);}, "Miles must be negative.");
+
+        int[] L = {134, 75, 21, 876};
+        assertEquals(c3.roadTrip(L), 3, 0.1, "Mileage should return number of days successfully driven");
 
 
          return getFailedCount();
 
          // does this work
     }
-}
-
-public class Group3_4TeslaTester extends BCATestScenario{
-    public int runTest() {
-        TeslaModelZ c1 = new TeslaModelZ();
-
-        // #13
-        assertThrows(IllegalArgumentException.class, () -> {c1.drive(-1);}, "Driving mileage cannot be negative.");
-        // #14
-        assertTrue(c1.canDrive(1), "canDrive should be true for positive values");
-        // #15
-        assertFalse(c1.canDrive(341), "Driving mileage should not be greater than 340 miles.")
-        // #16
-        c1.toString();
-        // #17
-        assertThrows(IllegalArgumentException.class, () -> {c1.driveAutonomously(-1);}
-
-        assertEquals(c1.getMileage(), 30, .1, "Mileage should be 30 after first drive.");
-
-        c1.drive(200);
-        assertEquals(c1.getMileage(), 230, .1, "Mileage should be 230 after second drive.");
-
-        assertEquals(c1.getRemainingRange(), c1.getFuelCapacity() * c1.getMPG() - 230, .1, "Remaining range of car not correct after driving twice.");
-
-        assertFalse(c1.canDrive(252), "Driving 252 should fail.");
-        assertTrue(c1.canDrive(251), "Driving 251 should succeed.");
-
-        c1.drive(251);
-        assertEquals(c1.getMileage(), 481, .1, "Mileage should be 481 after third drive.");
-
-        assertThrows(IllegalArgumentException.class, () -> {c1.drive(5);}, "Driving beyond empty should fail.");
-
-
-        return getFailedCount();
-
-        // does this work
-   }
 }
